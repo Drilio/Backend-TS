9587a2d5a05ae04193e351e24ed89ea18c687571
Antoine <antoine.roy@tetras-libre.fr>
2024-01-16 14:50:30 +0100

    model

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -8,12 +8,13 @@ import jwt from 'jsonwebtoken';
 const User = mongoose.model('user',usersSchema);
  const signup = async (req: Request, res: Response) =>{
     console.log(req.body);
-    const { login, password } = req.body;
-    console.log('login' ,login)
+    const {name, mail, password } = req.body;
+    console.log('name' ,name)
     try{
         const passwordHash = await hash(password, 10)
         const userData = {
-            login: login,
+            name: name,
+            mail: mail,
             password: passwordHash,
         }
         const newUser = new User(userData);
@@ -27,10 +28,9 @@ const User = mongoose.model('user',usersSchema);
             success: true,
             user:{
                 user_id: newUser.id,
-                login: login,
-                password: password,
-                authToken: authToken,
-            }
+                email: mail,
+                name: name,
+                authToken: authToken,            }
         })
 }catch(error){
         console.error(error);
@@ -41,11 +41,12 @@ const User = mongoose.model('user',usersSchema);
 const login = async (req: Request, res: Response) =>{
     console.log(req.body);
     try{
-        const user = await User.findOne({login: req.body.login})
+        const user = await User.findOne({mail: req.body.mail})
        console.log('user :', user);
         if(user === null){
-            return res.status(401).json({ message: 'Paire identifiant / mot de passe incorrect' });
+            return res.status(401).json({ message: 'Paire identifiant / mot de passe incorecte' });
         }else{
+            console.log('body data', req.body.password)
             const passwordCorrect = await compare(req.body.password, user.password);
             console.log("passwordCorrect",passwordCorrect)
             if(passwordCorrect){
@@ -56,8 +57,11 @@ const login = async (req: Request, res: Response) =>{
                 const authToken = jwt.sign(user.toObject(), process.env.AUTH_TOKEN_KEY!, jwtOptions);
                 return res.status(200).json({
                     success: true,
-                    data: {
-                        jwtToken: authToken,
+                    user: {
+                        user_id: user.id,
+                        email: user.mail,
+                        name: user.name,
+                        auth_token: authToken,
                     },
                 });
             }else{
@@ -65,33 +69,91 @@ const login = async (req: Request, res: Response) =>{
                 return res.status(400).json({error: 'Invalid Credentials'});
             }
         }
+        return res.status(400).json({error: 'Invalid Credentials'});
     }catch (error){
         console.error(error);
         return res.status(500).json({ error: `Server Error` });
     }
 }
 
+const me = async (req:Request,res:Response,next:NextFunction) => {
+     console.log(req.body)
+    try{
+        const authorization = req.headers.authorization!.split(' ')[1];
+        const decoded = jwt.verify(authorization, process.env.AUTH_TOKEN_KEY!) as IUser
+        console.log(decoded)
+        const userMail = decoded.mail
+        const user = await User.findOne({mail: userMail});
+         if(user == null){
+             res.status(401).json({ message: 'Incorrect Token' });
+         }else{
+             return res.status(200).json({
+                 success: true,
+                 user: {
+                     user_id: user.id,
+                     email: user.mail,
+                     name: user.name,
+                 },
+             });
+         }
+
+    }catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: `Server Error` });
+    }
+}
+
+const changePassword = async (req:Request,res:Response,next:NextFunction) => {
+try{
+    const authorization = req.headers.authorization!.split(' ')[1];
+    const decoded = jwt.decode(authorization) as IUser
+    const userMail = decoded.mail
+
+    const filter = {mail: userMail}
+const connectedUser = await User.findOne(filter);
+    if(connectedUser){
+        console.log(req.body, connectedUser.password)
+        const match = await compare(req.body.currentPassword ,connectedUser.password)
+        console.log(connectedUser);
+        console.log(match);
+        if(req.body.password === req.body.passwordConfirmation && match){
+            const user = await User.findOneAndUpdate(filter, {password: req.body.password},);
+            res.status(200).json({message:'votre mdp à bien changé !'})
+        }else{
+            res.status(401).json({ message: 'Invalid informations' });
+        }
+    }else{
+        res.status(401).json({ message: 'User does not exist' });
+    }
+
+}catch(error){
+    console.error(error);
+    return res.status(500).json({ error: `Server Error` });
+}
+}
 
 const isconnect = async (req:Request,res:Response,next:NextFunction) => {
-     // try{
-     //     console.log('isconnect test')
-     //     const authorization = req.headers.authorization!.split(' ')[1];
-     //     const decoded = jwt.decode(authorization) as IUser
-     //     const userMail = decoded.mail
-     //     const filter = {mail: userMail}
-     //     const connectedUser = await User.findOne(filter);
-     //     if(connectedUser === null){
-     //         res.status(401).json({ message: 'l\'utilisateur n\'existe pas' });
-     //     }else{
-     //         res.status(200).json({ message: 'Token valide' });
-     //     }
-     // }catch (error) {
-     //     res.status(200).json({ message: 'Token valide' });
-     // }
+     try{
+         console.log('isconnect test')
+         const authorization = req.headers.authorization!.split(' ')[1];
+         const decoded = jwt.decode(authorization) as IUser
+         const userMail = decoded.mail
+         const filter = {mail: userMail}
+         const connectedUser = await User.findOne(filter);
+         if(connectedUser === null){
+             res.status(401).json({ message: 'l\'utilisateur n\'existe pas' });
+         }else{
+             res.status(200).json({ message: 'Token valide' });
+         }
+     }catch (error) {
+         res.status(200).json({ message: 'Token valide' });
+     }
 }
 const controller = {
     signup,
     login,
+    me,
+    changePassword,
     isconnect
 }
 
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,8 +3,14 @@ import userCtrl from '../controllers/user';
 const router = express.Router();
 
 
-router.post('/add', userCtrl.signup);
-router.post('/connect', userCtrl.login);
+router.post('/signup', userCtrl.signup);
+
+router.post('/local', userCtrl.login);
+
+router.get('/me', userCtrl.me)
+
+router.post('/change-password', userCtrl.changePassword)
+
 router.post('/isconnect', userCtrl.isconnect)
 
 export default router
