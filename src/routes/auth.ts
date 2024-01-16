7aae932ea5a1ccf9a20529c1b2a60fa13e624bf6
Antoine <antoine.roy@tetras-libre.fr>
2024-01-16 14:43:25 +0100

    model

--- src/routes/auth.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import userCtrl from '../controllers/auth';
-const router = express.Router();
-
-
-router.post('/add', userCtrl.signup);
-router.post('/connect', userCtrl.login);
-export default router
